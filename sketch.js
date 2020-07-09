2be17d989952c1e591aa26a7cde72ed6270d7e84
Saankhya2378 <saankhyapix@gmail.com>
2020-07-09 13:00:56 -0400

    changes

--- sketch.js
+++ sketch.js
@@ -14,10 +14,9 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
-
-
-    ground = new Ground(600,height,1200,20);
+    
     platform = new Ground(150, 305, 300, 170);
+    ground = new Ground(600,height,1200,20);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -46,6 +45,7 @@ function draw(){
     console.log(box2.body.angle);
     box1.display();
     box2.display();
+    platform.display();
     ground.display();
     pig1.display();
     log1.display();
@@ -60,5 +60,4 @@ function draw(){
     log5.display();
 
     bird.display();
-    platform.display();
 }
